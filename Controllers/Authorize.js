32bdfe3fcdf4634a32f0e02f6595b34bbfcc4edf
Michelle Hoelzer <hoelzermm@gmail.com>
2019-01-15 20:53:36 -0500

    jsonwebtoken setup

--- Controllers/Authorize.js
+++ Controllers/Authorize.js
@@ -15,11 +15,11 @@ router.post("/register", (req, res) => {
     username,
     password: hashedPassword
   })
-    .then(user =>
+    .then(user => {
       res.json({
         username: user.get("username")
-      })
-    )
+      });
+    })
     .catch(error => {
       if (error instanceof Sequelize.ValidationError) {
         return res.status(400).send({ errors: error.errors });
@@ -39,9 +39,12 @@ router.post("/login", (req, res) => {
     .then(User => {
       if (User) {
         if (bcrypt.compareSync(password, User.get("password"))) {
+          let token = jwt.sign({username: User.get("username")}, process.env.JWT_SECRET, {
+            expiresIn: "24h"
+          });
           const payload = { id: User.get("id") };
           console.log(password);
-          res.json({ id: payload.id, username, success: true });
+          res.json({ id: payload.id, username, success: true, token });
         } else {
           res.json({ success: false }); // could add more detail about the password error
         }
@@ -52,7 +55,12 @@ router.post("/login", (req, res) => {
 });
 
 router.post("/logout", (req, res) => {
-  req.getlogout();
+//   req.getlogout();
+  try {
+    jwt.verify(req.headers.authorization.replace("Bearer ", ""), process.env.JWT_SECRET); 
+  } catch (err) {
+    return res.status(401).send();
+  }
   res.json({ success: true, message: "logged out" });
 });
 
