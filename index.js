1e0b8b0df7521712ac22d34fd70374c1b28ee25c
Michelle Hoelzer <hoelzermm@gmail.com>
2019-01-15 22:54:46 -0500

    creating notebook endpiiunts

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 
 app.use("/Authorize", controllers.authorize);
 // app.use("/Upload", controllers.books);
-// app.use("/User", controllers.user);
+app.use("/users", controllers.user);
 
 app.listen(app.get("port"), () => {
   console.log(`app listening on port ${app.get("port")}`);
