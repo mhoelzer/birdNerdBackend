29807c288a382f1a6f1614f6d31a3e81fe84f0a6
Michelle Hoelzer <hoelzermm@gmail.com>
2022-07-26 15:23:24 -0400

    changing to bcrypt to bcryptjs because why not

--- Controllers/Authorize.js
+++ Controllers/Authorize.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const Sequelize = require("sequelize");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
