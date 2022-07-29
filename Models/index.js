f287ed43e8c9ea0d5a5e49c2dc7c74fd05f777d9
Michelle Hoelzer <hoelzermm@gmail.com>
2022-07-29 11:55:56 -0400

    testing out sequelizerc

--- Models/index.js
+++ Models/index.js
@@ -1,10 +1,10 @@
 const Sequelize = require("sequelize");
-const aws = require('aws-sdk');
+// const aws = require('aws-sdk');
 
-let s3 = new aws.S3({
-  accessKeyId: process.env.S3_KEY,
-  secretAccessKey: process.env.S3_SECRET
-});
+// let s3 = new aws.S3({
+//   accessKeyId: process.env.S3_KEY,
+//   secretAccessKey: process.env.S3_SECRET
+// });
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const User = sequelize.import("./User");
